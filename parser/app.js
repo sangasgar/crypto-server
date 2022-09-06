6471f901c04295791cbbaa197162c25f52dc7c1b
sangasgar <sangas@yandex.ru>
2022-09-06 10:19:45 +0300

    Добавил локальное хранилище, вынес апи в отдельный файл

--- parser/app.js
+++ parser/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 require('dotenv').config();
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -7,21 +6,32 @@ const logger = require('morgan');
 const cors = require('cors');
 const indexRouter = require('./routes/index');
 const apiRouter = require('./routes/api');
+const playBot = require('./middleWare/apiBybit');
 
 const { PORT } = process.env || 3011;
+
 const app = express();
-
+const startBot = async () => {
+  await playBot();
+};
 app.use(logger('dev'));
-
+app.use(logger('dev'));
+app.use((req, res, next) => {
+  console.log('Called URL:', req.url);
+  next();
+});
 app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname)));
-
+startBot();
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
 app.listen(PORT, () => {
   console.log('Server запущен на порту ', PORT);
 });
+setInterval(async () => {
+  await playBot();
+}, 42000);
 module.exports = app;
---
+++ parser/middleWare/apiBybit.js
@@ -0,0 +1,108 @@
+const axios = require('axios');
+const { LinearClient } = require('bybit-api');
+const HeikinAshi = require('heikinashi');
+const tulind = require('tulind');
+/* eslint-disable max-len */
+const cipherB = require('../indicators/cipherB');
+const bullTv = require('../indicators/bullTv');
+const storage = require('../storage/storage');
+
+const { API_KEY } = process.env;
+const { PRIVATE_KEY } = process.env;
+const useLivenet = false;
+
+const client = new LinearClient(
+  API_KEY,
+  PRIVATE_KEY,
+
+  // optional, uses testnet by default. Set to 'true' to use livenet.
+  useLivenet,
+
+  // restClientOptions,
+  // requestLibraryOptions
+);
+
+const period = {
+  five: '5',
+  fifteen: '15',
+  firteen: '30',
+  onehour: '60',
+  twohour: '120',
+  sixhour: '360',
+};
+async function playBot() {
+  const today = Date.now();
+  console.log('bot start');
+  const data5Period = await axios.get(`https://api.bybit.com/derivatives/v3/public/kline?category=linear&symbol=BTCUSDT&interval=${period.five}&start=${today - Number(period.five) * 200 * 60 * 1000}&end=${today}&limit=200`);
+  const data5periodData = data5Period.data.result.list;
+  const period5Data = HeikinAshi(data5periodData.map((el) => ({
+    time: el[0] / 1000, open: Number(el[1]), high: Number(el[2]), low: Number(el[3]), close: Number(el[4]), volume: Number(el[5]),
+  })).reverse(), {
+    overWrite: false,
+    formatNumbers: false,
+    decimals: 4,
+    forceExactDecimals: false,
+  });
+  storage.addItem('period5Data', period5Data);
+  const data15Period = await axios.get(`https://api.bybit.com/derivatives/v3/public/kline?category=linear&symbol=BTCUSDT&interval=${period.fifteen}&start=${today - Number(period.fifteen) * 200 * 60 * 1000}&end=${today}&limit=200`);
+  const data15periodData = data15Period.data.result.list;
+  const period15Data = HeikinAshi(data15periodData.map((el) => ({
+    time: el[0] / 1000, open: Number(el[1]), high: Number(el[2]), low: Number(el[3]), close: Number(el[4]), volume: Number(el[5]),
+  })).reverse(), {
+    overWrite: false,
+    formatNumbers: false,
+    decimals: 4,
+    forceExactDecimals: false,
+  });
+  storage.addItem('period15Data', period15Data);
+  const data30mPeriod = await axios.get(`https://api.bybit.com/derivatives/v3/public/kline?category=linear&symbol=BTCUSDT&interval=${period.firteen}&start=${today - Number(period.firteen) * 200 * 60 * 1000}&end=${today}&limit=200`);
+  const data30mperiodData = data30mPeriod.data.result.list;
+  const period30mData = HeikinAshi(data30mperiodData.map((el) => ({
+    time: el[0] / 1000, open: Number(el[1]), high: Number(el[2]), low: Number(el[3]), close: Number(el[4]), volume: Number(el[5]),
+  })).reverse(), {
+    overWrite: false,
+    formatNumbers: false,
+    decimals: 4,
+    forceExactDecimals: false,
+  });
+  storage.addItem('period30mData', period30mData);
+  const data1hPeriod = await axios.get(`https://api.bybit.com/derivatives/v3/public/kline?category=linear&symbol=BTCUSDT&interval=${period.onehour}&start=${today - Number(period.onehour) * 200 * 60 * 1000}&end=${today}&limit=200`);
+  const data1hperiodData = data1hPeriod.data.result.list;
+  const period1hData = HeikinAshi(data1hperiodData.map((el) => ({
+    time: el[0] / 1000, open: Number(el[1]), high: Number(el[2]), low: Number(el[3]), close: Number(el[4]), volume: Number(el[5]),
+  })).reverse(), {
+    overWrite: false,
+    formatNumbers: false,
+    decimals: 4,
+    forceExactDecimals: false,
+  });
+  storage.addItem('period1hData', period1hData);
+  const bull = await bullTv(period1hData);
+  const bullWithtime = period1hData.map((el, i) => ({
+    time: el.time, bull: bull[i],
+  }));
+  const data2hPeriod = await axios.get(`https://api.bybit.com/derivatives/v3/public/kline?category=linear&symbol=BTCUSDT&interval=${period.twohour}&start=${today - Number(period.twohour) * 200 * 60 * 1000}&end=${today}&limit=200`);
+  const data2hperiodData = data2hPeriod.data.result.list;
+  const period2hData = HeikinAshi(data2hperiodData.map((el) => ({
+    time: el[0] / 1000, open: Number(el[1]), high: Number(el[2]), low: Number(el[3]), close: Number(el[4]), volume: Number(el[5]),
+  })).reverse(), {
+    overWrite: false,
+    formatNumbers: false,
+    decimals: 4,
+    forceExactDecimals: false,
+  });
+  storage.addItem('period2hData', period2hData);
+  const data6hPeriod = await axios.get(`https://api.bybit.com/derivatives/v3/public/kline?category=linear&symbol=BTCUSDT&interval=${period.sixhour}&start=${today - Number(period.sixhour) * 200 * 60 * 1000}&end=${today}&limit=200`);
+  const data6hperiodData = data6hPeriod.data.result.list;
+  const period6hData = HeikinAshi(data6hperiodData.map((el) => ({
+    time: el[0] / 1000, open: Number(el[1]), high: Number(el[2]), low: Number(el[3]), close: Number(el[4]), volume: Number(el[5]),
+  })).reverse(), {
+    overWrite: false,
+    formatNumbers: false,
+    decimals: 4,
+    forceExactDecimals: false,
+  });
+  storage.addItem('period6hData', period6hData);
+  console.log('down bot');
+}
+module.exports = playBot;
