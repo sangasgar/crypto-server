267e3822ff244f5d3c107d6b84c983d53b5744b9
sangasgar <sangas@yandex.ru>
2022-09-13 13:18:12 +0300

    Добавил новые проценты от депозита

--- client/src/components/Admin/BotPage/BotPage.jsx
+++ client/src/components/Admin/BotPage/BotPage.jsx
@@ -126,8 +126,15 @@ function BotPage() {
           ? (
             <select onChange={sizeDepositChangeHandler} name="sizeDeposit">
               <option disabled>{sizeDeposit}</option>
+              <option value="5">5</option>
               <option value="10">10</option>
+              <option value="15">15</option>
+              <option value="20">20</option>
               <option value="25">25</option>
+              <option value="30">30</option>
+              <option value="35">35</option>
+              <option value="40">40</option>
+              <option value="45">45</option>
               <option value="50">50</option>
               <option value="75">75</option>
               <option value="100">100</option>
