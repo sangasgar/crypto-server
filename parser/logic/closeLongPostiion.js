bcbfa8a35c7938a8d1e8164dc7b4d198b303bef0
sangasgar <sangas@yandex.ru>
2022-10-10 16:52:46 +0300

    Добавлена возможность выхода из сделки

--- parser/logic/closeLongPostiion.js
+++ parser/logic/closeLongPostiion.js
@@ -13,14 +13,14 @@ async function closeLongPosition(id, client, symbol) {
   const vwapLast = Number(period15DataCipherBwithTime[1].vwap);
   const vwapMax = Math.max(period15DataCipherBwithTime[1].vwap, period15DataCipherBwithTime[2].vwap, period15DataCipherBwithTime[3].vwap, period15DataCipherBwithTime[4].vwap);
   const currentVwap = period15DataCipherBwithTime[0].vwap;
-  let currentVwap30Change = false;
-  if (currentVwap < vwapMax - (vwapMax * 0.3)) {
-    currentVwap30Change = true;
+  let currentVwap50Change = false;
+  if (currentVwap <= vwapMax - (vwapMax * 0.5)) {
+    currentVwap50Change = true;
   }
   const positioByBit = await client.getPosition({ symbol });
   const positionSize = Number(positioByBit.result[0].size);
   if (positionSize > 0) {
-    if (vwapLast <= 3.5 || currentVwap30Change) {
+    if (vwapLast <= 3.5 || currentVwap50Change) {
       const closePosition = await client.placeActiveOrder({
         symbol, side: 'Sell', qty: positionSize, order_type: 'Market', close_on_trigger: false, reduce_only: true, sl_trigger_by: 'LastPrice', time_in_force: 'ImmediateOrCancel',
       });
