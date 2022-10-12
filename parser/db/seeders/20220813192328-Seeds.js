95e82d93621d57c033a0237cb579bf823b6a16c9
sangasgar <sangas@yandex.ru>
2022-10-12 10:59:12 +0300

    Добавлена стратегия шорт

--- parser/db/seeders/20220813192328-Seeds.js
+++ parser/db/seeders/20220813192328-Seeds.js
@@ -71,6 +71,66 @@ module.exports = {
       createdAt: new Date(),
       updatedAt: new Date(),
     }], {});
+    await queryInterface.bulkInsert('Users', [{
+      name: 'Garegin',
+      email: 'test@gmail.com',
+      password: await bcrypt.hash('123', Number(process.env.SALTROUNDS)),
+      privateKey: 'privateKey',
+      publicKey: 'publicKey',
+      leverage: 1,
+      stoploss: 10,
+      sizeDeposit: 20,
+      balance: 0,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }], {});
+    await queryInterface.bulkInsert('Positions', [{
+      symbol: 'ETHUSDT',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }], {});
+    await queryInterface.bulkInsert('Bots', [{
+      user_id: 3,
+      botStatus: false,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }], {});
+    await queryInterface.bulkInsert('PositionsUsers', [{
+      user_id: 3,
+      position_id: 3,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }], {});
+    await queryInterface.bulkInsert('Users', [{
+      name: 'Garegin',
+      email: 'test@yandex.com',
+      password: await bcrypt.hash('123', Number(process.env.SALTROUNDS)),
+      privateKey: 'privateKey',
+      publicKey: 'publicKey',
+      leverage: 1,
+      stoploss: 10,
+      sizeDeposit: 20,
+      balance: 0,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }], {});
+    await queryInterface.bulkInsert('Positions', [{
+      symbol: 'ETHUSDT',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }], {});
+    await queryInterface.bulkInsert('Bots', [{
+      user_id: 4,
+      botStatus: false,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }], {});
+    await queryInterface.bulkInsert('PositionsUsers', [{
+      user_id: 4,
+      position_id: 4,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }], {});
   },
 
   async down(queryInterface, Sequelize) {
